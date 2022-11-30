19c853ad02305f763c20a2dd536897043dafcd71
Gustavo Lira <gu.lirar23@gmail.com>
2022-11-29 23:52:38 -0600

    add two beans depends of spring.active.profile property

--- src/main/java/com/springexample/project/config/ApplicationConfig.java
+++ src/main/java/com/springexample/project/config/ApplicationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 import com.springexample.project.services.GreetingService;
@@ -29,8 +30,14 @@ public class ApplicationConfig {
         return new GreetingService(greeting);
     }
     @Bean
+    @Profile("!dev")
     public TimeService timeService(){
-        return new TimeService();
+        return new TimeService(false);
+    }
+    @Bean
+    @Profile("dev")
+    public TimeService timeService2(){
+        return new TimeService(true);
     }
     @Bean 
     public OutputService outputService() {
