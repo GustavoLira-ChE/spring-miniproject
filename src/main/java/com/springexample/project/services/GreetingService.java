6c757fb350231496986bc6caa90d7b0366933e8b
Gustavo Lira <gu.lirar23@gmail.com>
2022-11-30 11:19:54 -0600

    add Loggable custom annotation to apply the aspect

--- src/main/java/com/springexample/project/services/GreetingService.java
+++ src/main/java/com/springexample/project/services/GreetingService.java
@@ -3,6 +3,8 @@ package com.springexample.project.services;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.springexample.project.aspect.Loggable;
+
 @Service
 public class GreetingService {
 
@@ -12,6 +14,7 @@ public class GreetingService {
     public GreetingService(){
     }
 
+    @Loggable
     public String getGretting(String name){
         return this.greeting + " " + name;
     }
