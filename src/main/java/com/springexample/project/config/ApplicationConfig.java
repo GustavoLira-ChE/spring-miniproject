ccb32c9f58981aa858a0591afb015adc1c92b86d
Gustavo Lira <gu.lirar23@gmail.com>
2022-11-30 00:18:48 -0600

    delete bean definition and add to the class the ComponentScan annotation with the basesPackages set to com.springexample.project

--- src/main/java/com/springexample/project/config/ApplicationConfig.java
+++ src/main/java/com/springexample/project/config/ApplicationConfig.java
@@ -1,41 +1,12 @@
 package com.springexample.project.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-import com.springexample.project.services.GreetingService;
-import com.springexample.project.services.OutputService;
-import com.springexample.project.services.TimeService;
-
 @Configuration
 @PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.springexample.project")
 public class ApplicationConfig {
     
-    @Value("${app.greeting}")
-    private String greeting;
-    @Value("${app.name}")
-    private String name;
-    @Value("#{new Boolean(environment['spring.profiles.active'] != 'dev')}")
-    private boolean isTommorow;
-
-    @Autowired
-    private GreetingService greetingService;
-    @Autowired
-    private TimeService timeService;
-
-    @Bean
-    public GreetingService greetingService(){
-        return new GreetingService(greeting);
-    }
-    @Bean
-    public TimeService timeService(){
-        return new TimeService(isTommorow);
-    }
-    @Bean 
-    public OutputService outputService() {
-        return new OutputService(greetingService, timeService,name);
-    }
 }
