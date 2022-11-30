fa65c079121536d4fa95c4caa363e4094848f8be
Gustavo Lira <gu.lirar23@gmail.com>
2022-11-30 10:25:35 -0600

    change aspect directory to project directory

--- src/main/java/com/springexample/project/config/ApplicationConfig.java
+++ src/main/java/com/springexample/project/config/ApplicationConfig.java
@@ -2,11 +2,13 @@ package com.springexample.project.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.springexample.project")
+@EnableAspectJAutoProxy
 public class ApplicationConfig {
     
 }
