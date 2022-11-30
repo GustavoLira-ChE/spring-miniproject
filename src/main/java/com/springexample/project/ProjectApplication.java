a0273991ee29acdfe9c53e5611284a0c79f338f4
Gustavo Lira <gu.lirar23@gmail.com>
2022-11-29 22:34:52 -0600

    change springboot annotation and add ApplicationContext

---
+++ src/main/java/com/springexample/project/ProjectApplication.java
@@ -0,0 +1,22 @@
+package com.springexample.project;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.springexample.project.config.ApplicationConfig;
+import com.springexample.project.services.OutputService;
+
+public class ProjectApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+
+		OutputService outputService = context.getBean(OutputService.class);
+
+		for(int i = 0; i <= 5; i++){
+			System.out.println(outputService.getFullGreeting("Frank"));
+			Thread.sleep(5000);
+		}
+		((AnnotationConfigApplicationContext)context).close();
+	}
+}
